fa670fb0b1aa1f7b448b0113070a0221ea7dc3a8
vagrant <vagrant@localhost.localdomain>
2018-04-29 18:44:00 +0000

    first commit

--- manager/views.py
+++ manager/views.py
@@ -77,22 +77,60 @@ class ArticleListView(TemplateView):
     template_name = "article_list.html"
 
     def get(self, request, *args, **kwargs):
-        info(request.GET.get("thread_id"))
-        person = Person.objects.get(pk=self.request.user.id)
         context = super(ArticleListView, self).get_context_data(**kwargs)
-        articles = Article.objects.all()
+
+        # もしthread_idかpageが存在しなければ
+        if request.GET.get("thread_id") == "" or request.GET.get("thread_id") is None or request.GET.get("page") == "" or request.GET.get("page") is None:
+            return redirect('/error/')
+
+        # ページ番号
+        page = int(request.GET.get("page"))
+        # まずは全ての記事を取得
+        articles = Article.objects.filter(thread__id=request.GET.get("thread_id"))
+
+        # もし存在しないページ数を開いたら
+        if len(articles) <= (page-1)*5:
+            return redirect('/error/')
+        # 次のページ、最後のページ制御
+        context['start'] = True
+        context['end'] = True
+        # 最初のページの場合
+        if page == 1:
+            context['start'] = False;
+        # 最後のページの場合
+        elif (len(articles) // (page*5)) <= 0:
+            context['end'] = False
+        # ページ番号挿入
+        # もし記事数が25(5*5に満たない場合)
+        if len(articles)//5 < 5:
+            numbers = []
+            for i in range(len(articles)//5+1):
+                numbers.append(i+1)
+
+
+        # 次に表示する記事文だけ取得
+        articles = Article.objects.filter(thread__id=request.GET.get("thread_id"))[(page-1)*5:(page-1+1)*5]
+
+        info(len(articles) // 5)
         context['articles'] = articles
+        # thread_id埋め込み
         context['thread_id'] = request.GET.get("thread_id")
+        # ページ番号埋め込み
+        context['page'] = request.GET.get("page")
+        # ページ数挿入
+        context['numbers'] = numbers
         return render(self.request, self.template_name, context)
 
     def post(self, _, *args, **kwargs):
         person = Person.objects.get(pk=self.request.user.id)
         thread = Thread.objects.get(pk=self.request.POST['thread_id'])
+        # ページ番号取得
+        page = self.request.POST['page']
         title = self.request.POST['title']
         text = self.request.POST['text']
         Article.objects.create(person=person, thread=thread, title=title, text=text, insymd=datetime.datetime.today())
 
-        return redirect('/article_list/?thread_id=' + str(thread.id))
+        return redirect('/article_list/?thread_id=' + str(thread.id) + "&page=" + page)
 
 class ThreadListView(TemplateView):
     template_name = "thread_list.html"
